64434815bb5df267cf294f97880063db27902fd3
yogik23 <61941105+yogik23@users.noreply.github.com>
2024-10-06 19:23:49 +0700

    Create welcomeMessage.js

---
+++ welcomeMessage.js
@@ -0,0 +1,18 @@
+const welcomeMessage = `
+
+
+    ███████╗██╗  ██╗██╗    ██╗
+    ██╔════╝██║ ██╔╝██║    ██║
+    ███████╗█████╔╝ ██║ █╗ ██║
+    ╚════██║██╔═██╗ ██║███╗██║
+    ███████║██║  ██╗╚███╔███╔╝
+    ╚══════╝╚═╝  ╚═╝ ╚══╝╚══╝ 
+                          
+Inferium Inferno AutoBots by SKW AIRDROP HUNTER
+`;
+
+function displayWelcomeMessage() {
+  console.log(welcomeMessage);
+}
+
+module.exports = displayWelcomeMessage;
